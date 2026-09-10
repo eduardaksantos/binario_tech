b6698fb9c578d00c572a529d3eb2d5ab08aab414
Eduarda Kramer <eduarda.k.santos6@edu.senai.br>
2026-09-10 19:18:50 +0000

    feat: aula 16 -Prova (versao atualizada)

--- aula16/servidor_check.js
+++ aula16/servidor_check.js
@@ -13,8 +13,9 @@ app.get('/api/v1/status-servidor', (req,res) => {
 	});
 });
 
+// Versão Atualizada
 app.listen(PORT, () => {
-	console.log(`[Binário Tech] Servidor de Validação da Aula 16 ativo na porta ${PORT}`);
+	console.log(`[Binário Tech] Servidor de Validação da Aula 16 ativo na porta - Versão Atualizada ${PORT}`);
 	});
 
 
